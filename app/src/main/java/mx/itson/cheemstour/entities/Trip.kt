7baae900c6a2987a1b9f234f7ff2db8dbd1c6726
0Emiliano <emiliano.rascon22@outlook.com>
2026-04-16 18:56:13 -0700

    Initial commit

---
+++ app/src/main/java/mx/itson/cheemstour/entities/Trip.kt
@@ -0,0 +1,7 @@
+package mx.itson.cheemstour.entities
+
+class Trip {
+    constructor(id: Int, name: String, city: String, latitude: Double, longitude: Double)
+
+
+}
---
+++ app/src/main/java/mx/itson/cheemstour/utis/RetrofitUtil.kt
@@ -0,0 +1,7 @@
+package mx.itson.cheemstour.utis
+
+class RetrofitUtil {
+
+    //fun getApi()
+
+}
